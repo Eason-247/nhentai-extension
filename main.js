e19ecba1d3140e806514a05770c7621b6f4299ce
Eason247 <122990207+Eason-247@users.noreply.github.com>
2025-02-23 10:16:48 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         id: "open",
-        title: "open nhentai",
+        title: "open nhentai/x",
         contexts: ["selection"]
     });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "open") {
-        const targetURL = `https://nhentai.net/g/${info.selectionText.trim()}/`;
-        chrome.tabs.create({ url: targetURL });
+        if (info.selectionText.trim().charAt(0) == "@") {
+            const targetURL = `https://x.com/${info.selectionText.trim()}/`;
+            chrome.tabs.create({ url: targetURL }); //can't combine
+        }
+        else {
+            const targetURL = `https://nhentai.net/g/${info.selectionText.trim()}/`;
+            chrome.tabs.create({ url: targetURL }); //can't combine
         }
     }
-);
-
+});
